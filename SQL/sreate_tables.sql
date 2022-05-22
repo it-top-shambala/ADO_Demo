9afab7b1b07c88d43359265b1dc2be4c01163397
Starinin Andrey <starininandrey@gmail.com>
2022-05-22 11:22:01 +0300

    Add sql

---
+++ SQL/sreate_tables.sql
@@ -0,0 +1,56 @@
+﻿CREATE TABLE tab_roles
+(
+    id   INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    role TEXT NOT NULL
+);
+
+CREATE TABLE tab_accounts
+(
+    id       INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    login    TEXT NOT NULL,
+    password TEXT NOT NULL,
+    role_id  INT  NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES tab_roles (id)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+CREATE TABLE tab_users
+(
+    id         INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name TEXT NOT NULL,
+    last_name  TEXT NOT NULL,
+    FOREIGN KEY (id) REFERENCES tab_accounts (id)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+CREATE TABLE tab_user_emails
+(
+    id      INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id INT  NOT NULL,
+    email   TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES tab_users (id)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+INSERT INTO tab_roles(role)
+VALUES ('guest'),
+       ('user'),
+       ('admin');
+
+INSERT INTO tab_accounts(login, password, role_id)
+VALUES ('_guest', '123', 1),
+       ('anna', '12345', 2),
+       ('ad', '123', 3);
+
+INSERT INTO tab_users(first_name, last_name)
+VALUES ('Anonim', 'Anonimus'),
+       ('Anna', 'Karenina'),
+       ('Admin', 'Adminus');
+
+INSERT INTO tab_user_emails(user_id, email)
+VALUES (2, 'anna@karenin.ru'),
+       (3, 'admin@admin.ru'),
+       (3, 'ad@admin.ru');
