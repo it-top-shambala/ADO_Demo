29cdfa9a64afa07affb82642d9fbba58e506f76a
Starinin Andrey <starininandrey@gmail.com>
2022-05-22 14:14:02 +0300

    Update

--- ADO_Demo.App/MainWindow.xaml.cs
+++ ADO_Demo.App/MainWindow.xaml.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ADO_Demo.DB.CRUDs;
+using ADO_Demo.DB.Models;
 
 namespace ADO_Demo.App
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Role> Roles { get; set; }
         public MainWindow()
         {
+            var roles = new RoleCrud();
+            Roles = new ObservableCollection<Role>(roles.GetAllRoles());
+            
             InitializeComponent();
         }
     }
