b70ba1900c1ef19978a1615ca8923c74bc2b46ef
Starinin Andrey <starininandrey@gmail.com>
2022-05-22 12:42:28 +0300

    Add MySql.Data

--- ADO_Demo.DB/DataBase.cs
+++ ADO_Demo.DB/DataBase.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
 
 namespace ADO_Demo.DB
 {
     public class DataBase
     {
+        private MySqlConnection _db;
+        private const string Str = "Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;";
+
+        public DataBase()
+        {
+            _db = new MySqlConnection(Str);
+        }
+        
         
     }
 }
