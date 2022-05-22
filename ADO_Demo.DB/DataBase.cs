053e3303e43e8cba5d0a5bbdd8fd76bc799216a0
Starinin Andrey <starininandrey@gmail.com>
2022-05-22 14:00:13 +0300

    Update DataBase.cs (add ConfigDb)

--- ADO_Demo.DB/DataBase.cs
+++ ADO_Demo.DB/DataBase.cs
@@ -1,16 +1,21 @@
-﻿using MySql.Data.MySqlClient;
+﻿using System.Data;
+using System.IO;
+using ADO_Demo.DB.Config;
+using MySql.Data.MySqlClient;
 
 namespace ADO_Demo.DB
 {
     public abstract class DataBase
     {
-        private const string Str = "";
         protected readonly MySqlConnection _db;
         protected MySqlCommand _command;
 
         protected DataBase()
         {
-            _db = new MySqlConnection(Str);
+            using var file = new StreamReader("config_db.json");
+            var config = ConfigDb.ImportFromJson(file.ReadToEnd());
+            
+            _db = new MySqlConnection(config.ToString());
             _command = new MySqlCommand { Connection = _db };
         }
     }
