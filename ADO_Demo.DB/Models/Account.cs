59b7dda8953657d4fc989d185087c6d401698722
Kras_wo29@itstepru.onmicrosoft.com <Kras_wo29@itstepru.onmicrosoft.com>
2022-05-27 20:43:48 +0500

    Include Account, AccountCrud

---
+++ ADO_Demo.DB/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_Demo.DB.Models
+{
+    public record Account
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int Role_Id { get; set; }
+
+    }
+}
