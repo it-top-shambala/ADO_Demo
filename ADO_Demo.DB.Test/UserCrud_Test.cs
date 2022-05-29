91e704178c0c4c2d601aa6612884a6be6e71e6a4
Starinin Andrey <starininandrey@gmail.com>
2022-05-29 11:13:15 +0300

    Add UserCrud_Test

---
+++ ADO_Demo.DB.Test/UserCrud_Test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using ADO_Demo.DB.CRUDs;
+using ADO_Demo.DB.Models;
+using Xunit;
+
+namespace ADO_Demo.DB.Test
+{
+    public class UserCrudTest
+    {
+        [Fact]
+        public void GetAllUsers_Test()
+        {
+            var expectedUsers = new List<User>
+            {
+                new()
+                {
+                    Id = 1,
+                    FirstName = "Anonim",
+                    LastName = "Anonimus"
+                },
+                new()
+                {
+                    Id = 2,
+                    FirstName = "Anna",
+                    LastName = "Karenina"
+                },
+                new()
+                {
+                    Id = 3,
+                    FirstName = "Admin",
+                    LastName = "Adminus"
+                },
+            };
+
+            var userCrud = new UserCrud();
+            var actualUsers = userCrud.GetAllUsers();
+            
+            Assert.Equal(expectedUsers, actualUsers);
+        }
+    }
+}
