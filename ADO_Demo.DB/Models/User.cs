569f7a565de025fbf166fec1cee046f29a8a25e1
Starinin Andrey <starininandrey@gmail.com>
2022-05-29 11:12:50 +0300

    Add User

---
+++ ADO_Demo.DB/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ADO_Demo.DB.Models
+{
+    public record User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
