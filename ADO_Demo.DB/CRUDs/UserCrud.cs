55f7628586459675257e2ca1d82f8386884e3e29
Starinin Andrey <starininandrey@gmail.com>
2022-05-29 11:13:02 +0300

    Add UserCrud

---
+++ ADO_Demo.DB/CRUDs/UserCrud.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using ADO_Demo.DB.Models;
+using Dapper;
+
+namespace ADO_Demo.DB.CRUDs
+{
+    public class UserCrud : DataBase
+    {
+        public UserCrud() : base()
+        {
+            Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            _db.Open();
+            var sql = "SELECT * FROM tab_users";
+            var result = _db.Query<User>(sql);
+            _db.Close();
+            return result;
+        }
+    }
+}
